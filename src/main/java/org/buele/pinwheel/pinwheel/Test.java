a90109b3f39c88b8fec54917c56a9dd9be9ae84f
Raffaele Bua <raffaele.bua@gmail.com>
2014-12-23 14:30:27 +0100

    Revert "Commit message"

--- src/main/java/org/buele/pinwheel/pinwheel/Test.java
+++ src/main/java/org/buele/pinwheel/pinwheel/Test.java
@@ -41,6 +41,5 @@ public class Test extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
-	//
 
 } {{{
