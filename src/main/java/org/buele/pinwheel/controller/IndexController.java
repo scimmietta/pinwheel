498861767b79ea9bfb8dd516baa02b17e541f5f0
rafbua <raffaele.bua@gmail.com>
2014-12-28 12:04:52 +0100

    fixed servlet-mapping

---
+++ src/main/java/org/buele/pinwheel/controller/IndexController.java
@@ -0,0 +1,17 @@
+package org.buele.pinwheel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+ 
+@Controller
+@RequestMapping("/index.html")
+public class IndexController{
+	@RequestMapping(method=RequestMethod.GET)
+	public String hello(ModelMap model) {
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "index";
+	}
+}
